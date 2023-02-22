114e26875c1683e1a92c5632e381f37d903d0ce0
RafaelGonçalves <83875972+RafaelGoncalves-bit@users.noreply.github.com>
2023-02-22 14:35:41 -0300

    Add files via upload

---
+++ aluramidi-curso-arquivos-iniciais/main.js
@@ -0,0 +1,49 @@
+function tocaSom (idElementoAudio) {
+  const elemento = document.querySelector(idElementoAudio);
+
+
+  if (elemento && elemento.localName === 'audio') {
+        elemento.play();
+  }
+        
+     else {
+        console.log('Elemento não encontrado')
+     }
+
+}
+
+
+
+
+const ListaDeTeclas = document.querySelectorAll('.tecla');
+
+
+
+//para
+for (let contador = 0; contador < ListaDeTeclas.length; contador++) {
+
+const tecla = ListaDeTeclas[contador];
+const instrumento = tecla.classList[1];
+const idAudio = `#som_${instrumento}`;
+
+    
+    tecla.onclick = function () {
+        tocaSom (idAudio);
+    }
+
+    tecla.onkeydown = function (evento) {
+
+
+    if (evento.code === 'Space' || evento.code === 'Enter'){
+        tecla.classList.add('ativa')
+    }
+
+}
+
+    tecla.onkeyup = function() {
+        tecla.classList.remove('ativa')
+    }
+  
+
+}
+
